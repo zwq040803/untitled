22dfb7cba84c99c99b3d76514b5a0c6fc742e91d
zwq <2780469203@qq.com>
2024-05-09 15:39:00 +0800

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void mian(String[] args){
+        System.out.println("姓名: ");
+        System.out.println("班级: ");
+        System.out.println("学号: ");
+
+    }
+}
